ecf6e4f7999345a5264f9ebf1be83431a21965bf
DanilaK <m98ail@icloud.com>
2018-10-21 02:55:33 +0300

    Creat new project.

---
+++ Practical-1/Practical-1/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class EntryPoint
+    {
+        public static void Main()
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error : {e.Message}");
+            }
+        }
+    }
+}
