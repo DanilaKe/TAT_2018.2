ed16549a3447a0ccebc102ddcaa263bec46fe2a5
DanilaK <m98ail@icloud.com>
2018-10-21 03:10:00 +0300

    Create Node class.

--- Practical-1/Practical-1/Node.cs
+++ Practical-1/Practical-1/Node.cs
@@ -7,29 +7,34 @@
     /// </summary>
     internal class Node<TClass>
     {
+        private TClass Value { get; set; }
+        private readonly Node<TClass> _next;
+
         /// <summary>
-        /// 
+        /// Constructor one node of stack. 
         /// </summary>
-        /// <param name="addedElement"></param>
-        /// <param name="nextElement"></param>
         public Node(TClass addedElement, Node<TClass> nextElement)
         {
+            Value = addedElement;
+            _next = nextElement;
         }
 
         /// <summary>
-        /// 
+        /// Method GetValue
+        /// Return value of one node.
         /// </summary>
-        /// <returns></returns>
         public TClass GetValue()
         {
+            return Value;
         }
         
         /// <summary>
-        /// 
+        /// Method GetNext
+        /// Return link to the next node.
         /// </summary>
-        /// <returns></returns>
         public Node<TClass> GetNext()
         {
+            return _next;
         }
     }
 }
