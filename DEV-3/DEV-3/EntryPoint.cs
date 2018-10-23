2913c1c5ac2d200f861da307dcb6054ea15f246d
DanilaK <m98ail@icloud.com>
2018-10-24 00:43:28 +0300

    First working version.

---
+++ DEV-3/DEV-3/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DEV_3
+{
+    internal class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            NumbersConverter a = new NumbersConverter();
+            Console.WriteLine(a.ConvertNumberFromDecimal(26515613, 20));
+        }
+    }
+}
