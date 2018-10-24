6c29a9eb4a423070711bdc8886d5a986257e6d24
DanilaK <m98ail@icloud.com>
2018-10-24 23:19:25 +0300

    Work version.

--- DEV-4/DEV-4/EntryPoint.cs
+++ DEV-4/DEV-4/EntryPoint.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace DEV_4
@@ -10,57 +13,90 @@ namespace DEV_4
         {
             try
             {
-                string xml = "<a>" +
-                             "dssfds" +
-                             "<b>" +
-                             "adasd" +
-                             "</b>" +
-                             "das" +
-                             "</a>";
+                string xml;
+                using (FileStream fstream = File.OpenRead("./DEV-4/Test.xml"))
+                {
+                    byte[] array = new byte[fstream.Length];
+                    fstream.Read(array, 0, array.Length);
+                    xml = System.Text.Encoding.Default.GetString(array);
+                }
                 Stack<string> a= new Stack<string>();
-                bool tagFlag =false;
+                bool tagFlag = false;
+                bool endFlag = false;
+                bool argumetFlag = false;
                 StringBuilder str = new StringBuilder();
                 for (var i = 0; i < xml.Length; i++)
                 {
-                    if (i == '<' && tagFlag)
+                    if ((xml[i] == ' ' && (!tagFlag && !argumetFlag))|| xml[i] == '\n')
+                    {
+                        continue;
+                    }
+                    if (xml[i] == '<' && tagFlag)
                     {
                         throw new Exception();
                     }
 
-                    if (i == '<')
+                    if (xml[i] == '<')
                     {
+                        argumetFlag = false;
                         if (str.ToString() != String.Empty)
                         {
-                            // ToDo
+                            StringBuilder bu = new StringBuilder();
+                            var Buffer = a.ToArray();
+                            for (var j = Buffer.Length-1; j >= 0; --j)
+                            {
+                                bu.Append($"{Buffer[j]} -> ");
+                            }
+
+                            bu.Append(str);
+                            Console.WriteLine(bu.ToString());
                             str.Clear();
                         }
 
-                        if (xml[Array.IndexOf(xml.ToCharArray(), i) + 1] == '/')
+                        if (xml[i+1] == '/')
                         {
-                            a.Pop();
+                            i++;
+                            endFlag = true;
                         }
 
                         tagFlag = true;
                         continue;
                     }
 
-                    if (i == '>')
+                    if (xml[i] == '>')
                     {
                         if (!tagFlag)
                         {
                             throw new Exception();
                         }
 
+                        if (endFlag)
+                        {
+                            var k = str.ToString();
+                            var s = new string(a.Peek().TakeWhile(x => x != ' ').ToArray());
+                            if (s != k)
+                            {
+                                throw new Exception();
+                            }
+
+                            tagFlag = false;
+                            endFlag = false;
+                            a.Pop();
+                            str.Clear();
+                            continue;
+                        }
                         tagFlag = false;
                         a.Push(str.ToString());
                         str.Clear();
                         continue;
                     }
 
-                    str.Append(i);
+                    if (!tagFlag)
+                    {
+                        argumetFlag = true;
+                    }
+                    str.Append(xml[i]);
                 }
-               
-
                 foreach (var i in a.ToArray())
                 {
                     Console.WriteLine(i);    
