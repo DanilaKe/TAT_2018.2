45b6f435f863579286a0f8acacd538e66a6c5951
DanilaK <m98ail@icloud.com>
2018-12-24 06:35:00 +0300

    Review PageFactory. Added waiter.

--- DEV-9/DEV-9/EntryPoint.cs
+++ DEV-9/DEV-9/EntryPoint.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Threading;
 using DEV_9.Pages;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
 
 namespace DEV_9
 {
@@ -21,7 +24,6 @@ namespace DEV_9
             try
             {
                 IWebDriver driver = new ChromeDriver("./DEV-9");
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
                 driver.Manage().Window.Maximize();
                 driver.Navigate().GoToUrl("https://vk.com/");
                 
@@ -33,13 +35,10 @@ namespace DEV_9
                 pageLogin.TxtPassword.SendKeys("password");
                 pageLogin.BtnLogin.Click();
                 
-                var pageFeed = new PageFeed();
-                PageFactory.InitElements(driver, pageFeed);
+                var pageFeed = new PageFeed(driver);
                 pageFeed.BtnMessage.Click();
                 
-                var pageMessage = new PageMessage();
-                PageFactory.InitElements(driver, pageMessage);
-
+                var pageMessage = new PageMessage(driver);
                 foreach (var message in pageMessage.UnreadMessages)
                 {
                     Console.WriteLine(message.Text);
--- DEV-9/DEV-9/Pages/PageFeed.cs
+++ DEV-9/DEV-9/Pages/PageFeed.cs
@@ -1,5 +1,7 @@
+using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
 
 namespace DEV_9.Pages
 {
@@ -9,10 +11,18 @@ namespace DEV_9.Pages
     /// </summary>
     public class PageFeed
     {
+        [FindsBySequence]
         [FindsBy(How = How.XPath, Using = "//*[@id=\"side_bar_inner\"]")]
-        public IWebElement MenuBar { get; set; }
-
-        [FindsBy(How = How.XPath, Using = "//*[@id=\"l_msg\"]")]
+        [FindsBy(How = How.CssSelector, Using = "#l_msg > a > span > span.left_label.inl_bl")]
         public IWebElement BtnMessage { get; set; }
+
+        public PageFeed(IWebDriver webDriver)
+        {
+            // Using PageFactory.
+            PageFactory.InitElements(webDriver, this);
+            var wait = new WebDriverWait(webDriver,TimeSpan.FromSeconds(60));
+            // Custom expected condition.
+            wait.Until((d) => BtnMessage.Displayed ? BtnMessage : null);
+        }
     }
 }
